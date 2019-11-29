aacdc2ef0d73ccb5cd7ad0012e76a0194a539512
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-29 14:34:03 -0500

    css switch

--- site.js
+++ site.js
@@ -1,9 +1,18 @@
 console.log("test");
 
 
-//// modal popup for 1-2-
 
-//////  1  resume download - pdf
+
+
 //////  2   business card - links to myspace - careerbuilder - aim - geocities - fax # 
 
 ///// coming soon
+
+
+document
+  .getElementById("themeSwitch")
+  .addEventListener("change", function(event) {
+    event.target.checked
+      ? document.body.setAttribute("data-theme", "light")
+      : document.body.removeAttribute("data-theme");
+  });
