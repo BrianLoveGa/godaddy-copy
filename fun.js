e1d16d96f294a331c234d7313e42949ac8a105b6
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-20 21:46:47 -0500

    json in console log

--- fun.js
+++ fun.js
@@ -31,10 +31,33 @@ function mUp(obj) {
   obj.innerHTML = "Thank You very much for that scratch";
 }
 
-
-getRMDate = async (e) => {
-    e.preventDefault();
-    const rand = Math.floor(Math.random()*490)
-}
-
+console.log("RM TESTING");
 /// https://rickandmortyapi.com/documentation/#javascript
+
+getRMDate = async => {
+  const rand = Math.floor(Math.random() * 490);
+  const urlRM = `https://rickandmortyapi.com/api/character/${rand}`;
+  console.log(urlRM);
+  fetch(urlRM)
+    .then(res => res.json())
+    .then(res => console.log("success!", res))
+    // .then(res => console.log(res.name))
+    // .then(document.getElementById('rmName').innerHTML='res.name')
+    .catch(err => console.log("something went wrong...", err));
+};
+
+getPokeDate = async => {
+  const randPm = Math.floor(Math.random() * 360);
+  const urlPm = `https://pokeapi.co/api/v2/pokemon/${randPm}`;
+  console.log(urlPm);
+  fetch(urlPm)
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      console.log("success!", res);
+    })
+    .catch(err => {
+      console.log("something went wrong...", err);
+    });
+};
